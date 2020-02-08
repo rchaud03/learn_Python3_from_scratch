a55eea1bce4b20d4780dd40a9ae450b6f47d8845
my Default <rchaud03@gmail.com>
2020-02-08 00:02:41 -0500

    february 7th 2358. Did Homework 7. May need to revise

--- learn_Python3/07.6-Homework7-2.py
+++ learn_Python3/07.6-Homework7-2.py
@@ -2,12 +2,13 @@ def net_income(my_gross, my_state):
     #print("Your gross is %s and you live in %s" %(my_gross, my_state))
 
     state_tax = {"FL": 0, "GA": .04, "TN": .07, "AL": .04, "LA": .04, "SC": .06}
-    net = my_gross - (my_gross * .1)
+
+    net_fed = my_gross - (my_gross * .1)
 
     if my_state in state_tax:
-        net = net - (net * state_tax[my_state])
-        print(" Your net income after state taxes is %s." % net)
-        return net
+        net_state = net_fed - (net_fed * state_tax[my_state])
+        print(" Your net income after state taxes is %s." % net_state)
+        return net_state
     else:
         print("State %s is not in our list of states")
         return None
