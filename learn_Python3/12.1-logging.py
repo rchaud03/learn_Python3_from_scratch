60364b8fc627e9c2a2496de3473259ddcc37eee7
my Default <rchaud03@gmail.com>
2020-02-28 14:45:38 -0500

    testing logging

---
+++ learn_Python3/12.1-logging.py
@@ -0,0 +1,7 @@
+import logging
+
+#logging.basicConfig(filename="12.1-logging_test.log", level=logging.DEBUG)
+logging.basicConfig(filename="12.1-logging_test.2.log", level=logging.WARNING)
+logging.warning("warning message")
+logging.info("info message")
+logging.error("error message")
---
+++ learn_Python3/12.2-logging_format.py
@@ -0,0 +1,15 @@
+"""
+Logging Format
+https://docs.python.org/3/library/logging.html#logrecord-attributes
+https://docs.python.org/3/library/time.html#time.strftime
+"""
+import logging
+
+#logging.basicConfig(format='%(asctime)s: %(levelname)s: %(message)s',
+#adding log-gile to prit to
+logging.basicConfig(filename= "12.2-logfile.log", format='%(asctime)s: %(levelname)s: %(message)s',
+                    datefmt='%m/%d/%Y %I:%M:%S %p',level=logging.DEBUG)
+                   # datefmt='%m/%d/%Y %I:%M:%S %p',level=logging.DEBUG)
+logging.warning("warning message")
+logging.info("info message")
+logging.error("error message")
